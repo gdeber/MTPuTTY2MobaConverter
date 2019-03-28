60facfdf9b5d0774df9c97186f9147156e2d57ff
gdeber <gdeber@libero.it>
2019-03-28 08:34:37 +0100

    Update MTPuTTY2MobaConverter.ps1
    Get rid of $root var.

--- MTPuTTY2MobaConverter.ps1
+++ MTPuTTY2MobaConverter.ps1
@@ -9,8 +9,6 @@ function ParseNode {
     $hostTemplate = "@@displayname@@=#109#0%@@hostname@@%22%%%-1%-1%%%22%%0%-1%0%%%-1%0%0%0%%1080%%0%0%1#MobaFont%10%0%0%0%15%248,248,242%39,40,34%255,128,0%0%-1%0%%xterm%-1%6%0,0,0%85,85,85%249,38,114%221,66,120%166,226,46%157,197,75%230,219,116%209,202,137%102,217,239%122,204,218%174,129,255%179,146,239%0,217,217%200,240,240%245,222,179%255,255,255%80%24%0%1%-1%<none>%%0#0#"
     
     if ($configNode.Type -eq 1) {
-        #sono un commento?
-        #il nodo è un host
         $outputText += $hostTemplate -replace "@@hostname@@", $configNode.ServerName `
                                  -replace "@@displayname@@", $configNode.DisplayName
         $outputText += "`r`n"
@@ -36,7 +34,6 @@ function ParseNode {
 }
 
 [xml]$mtputtyConfFile =[xml](Get-Content .\mtputty.xml)
-[System.Xml.XmlElement]$root = $mtputtyConfFile.get_DocumentElement()
 [System.Xml.XmlElement]$node = $null
 
 New-Variable -Name outputText
@@ -45,10 +42,10 @@ $outputText += "[Bookmarks]`r`n"
 $outputText += "SubRep=`r`n"
 $outputText += "ImgNum=42`r`n"
 
-foreach ($node in $root.Servers.Putty.Node) {
+foreach ($node in $mtputtyConfFile.Servers.Putty.Node) {
 
     $outputText = ParseNode $node $outputText $nodePath ([ref]$bookmarkNr)
 	
 }
 
-$outputText
+write-output $outputText
